7f12d4becb1b1984f76a5df51d21618e0fd02af4
Andrew Kiselev <andrew.kiselev92@gmail.com>
2019-09-27 16:39:09 +0300

    Updates

---
+++ precache-manifest.e591c96c4df552784d56091c131bd0cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fd29b24c2cef784b2289e09e01bd38",
+    "url": "/keyboard-trainer/index.html"
+  },
+  {
+    "revision": "37a3da28be47951fb4cf",
+    "url": "/keyboard-trainer/static/css/main.c0c7a18d.chunk.css"
+  },
+  {
+    "revision": "66f395d302fbe0e167c4",
+    "url": "/keyboard-trainer/static/js/2.b6fe4141.chunk.js"
+  },
+  {
+    "revision": "37a3da28be47951fb4cf",
+    "url": "/keyboard-trainer/static/js/main.d3716ac0.chunk.js"
+  },
+  {
+    "revision": "406d36291cfe020e6c77",
+    "url": "/keyboard-trainer/static/js/runtime~main.da4e7c66.js"
+  }
+]);
